915a5eae68284520d10ca4ef0e537cd65bccf659
shneor mizrachi <shneor146@gmail.com>
2024-08-25 03:28:51 +0300

    from the past

---
+++ Super Tic Tac Toe/View/TicTacToeView.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace Super_Tic_Tac_Toe.View;
+
+/// <summary>
+/// Interaction logic for TicTacToeView.xaml
+/// </summary>
+public partial class TicTacToeView : Page
+{
+    public TicTacToeView()
+    {
+        InitializeComponent();
+    }
+}
